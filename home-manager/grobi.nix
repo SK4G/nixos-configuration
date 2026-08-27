cdfdced2db9ea710dd82ebe863aabf7c8fe559a0
Luiz Salazar <luizsalazar87@gmail.com>
2026-08-27 10:51:10 -0500

    refactor(ui): update theme to Vapor and improve icon inheritance
    Remove redundant theme configurations from grobi.nix and migrate them
    to theme.nix. Switch the GTK theme from Arc-Dark to Vapor.
    
    Improve the Sardi-Arc icon theme by explicitly setting inheritance to
    breeze-dark, breeze, and hicolor via xdg.dataFile. Additionally,
    map clipman-symbolic to a Breeze-Dark icon to ensure visual consistency.

--- home-manager/grobi.nix
+++ home-manager/grobi.nix
@@ -5,37 +5,6 @@
     grobi
   ];
 
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-    XCURSOR_THEME = "Bibata-Modern-Ice";
-  };
-  
-  xresources.properties = {
-    "Xft.dpi" = 144;
-    "Xcursor.theme" = "Bibata-Modern-Ice";
-  };
-  xsession = {
-    initExtra = "xrdb -merge ~/.Xresources";
-    # numlock.enable = true;
-  };
-  
-  gtk = {
-    enable = true;
-    theme.name = "Arc-Dark";
-    cursorTheme.name = "Bibata-Modern-Ice";
-    iconTheme.name = "Sardi-Arc";
-    # iconTheme.name = "breeze-dark";
-  };
-
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-      gtk-theme = "Arc-Dark";
-      cursor-theme = "Bibata-Modern-Ice";
-      icon-theme = "Sardi-Arc";
-    };
-  };
-
   services.grobi = {
     enable = true;
     rules = [
--- home-manager/theme.nix
+++ home-manager/theme.nix
@@ -27,44 +27,86 @@
   
   gtk = {
     enable = true;
-    theme.name = "Arc-Dark";
+    theme.name = "Vapor";
     cursorTheme.name = "Bibata-Modern-Ice";
     iconTheme.name = "Sardi-Arc";
-    # iconTheme.name = "breeze-dark";
   };
 
   dconf.settings = {
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
-      gtk-theme = "Arc-Dark";
+      gtk-theme = "Vapor";
       cursor-theme = "Bibata-Modern-Ice";
       icon-theme = "Sardi-Arc";
     };
   };
 
-  # gtk = {
-  #   enable = true;
-  #   iconTheme = {
-  #     package = pkgs.arc-icon-theme;
-  #     name = "Arc";
-  #   };
-  #   theme = {
-  #     package = pkgs.arc-theme;
-  #     name = "Arc-Dark";
-  #   };
-  #   gtk3.extraConfig = {
-  #     gtk-application-prefer-dark-theme = true;
-  #   };
-  #   # font = {
-  #   #   name = "Ubuntu";
-  #   #   size = 10;
-  #   # };
-  # };
-  #   home.pointerCursor = {
-  #   name = "Vanilla-DMZ-AA";
-  #   size = 16;
-  #   package = pkgs.vanilla-dmz;
-  #   x11.enable = true;
-  #   gtk.enable = true;
-  # };
+  # Ensure Sardi-Arc inherits breeze-dark for toolbar and action icons
+  xdg.dataFile."icons/Sardi-Arc/index.theme".text = ''
+    [Icon Theme]
+    Name=Sardi Arc
+    Comment=Simple and flat icon theme with long shadow
+    Inherits=breeze-dark,breeze,hicolor
+    Example=folder
+
+    DisplayDepth=32
+    DesktopDefault=48
+    DesktopSizes=16,22,32,48,64,128,256
+    ToolbarDefault=22
+    ToolbarSizes=16,22,32,48
+    MainToolbarDefault=22
+    MainToolbarSizes=16,22,32,48
+    SmallDefault=16
+    SmallSizes=16,22,32,48
+    PanelDefault=32
+    PanelSizes=16,22,32,48,64,128,256
+    DialogDefault=32
+    DialogSizes=16,22,32,48,64,128,256
+
+    Directories=places/16,places/22,places/24,places/32,places/48,places/64,places/96,places/128
+
+    [places/16]
+    Size=16
+    Context=Places
+    Type=Fixed
+
+    [places/22]
+    Size=22
+    Context=Places
+    Type=Fixed
+
+    [places/24]
+    Size=24
+    Context=Places
+    Type=Fixed
+
+    [places/32]
+    Size=32
+    Context=Places
+    Type=Fixed
+
+    [places/48]
+    Size=48
+    Context=Places
+    Type=Fixed
+
+    [places/64]
+    Size=64
+    Context=Places
+    Type=Fixed
+
+    [places/96]
+    Size=96
+    Context=Places
+    Type=Fixed
+
+    [places/128]
+    Size=128
+    Context=Places
+    Type=Fixed
+  '';
+
+  # Map clipman-symbolic to Breeze-Dark's white paste icon
+  xdg.dataFile."icons/hicolor/scalable/apps/clipman-symbolic.svg".source =
+    "${pkgs.kdePackages.breeze-icons}/share/icons/breeze-dark/actions/16/edit-paste-symbolic.svg";
 }
